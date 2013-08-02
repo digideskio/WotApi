495d3d7b78878a6e520c7ac5754c3e2851cc0898
akeinhell <akeinhell@gmail.com>
2013-08-02 14:09:55 +0900

    Start project
    Signed-off-by: akeinhell <akeinhell@gmail.com>

---
+++ wotapi.php
@@ -0,0 +1,19 @@
+<?
+ 
+class wotapi{
+	function search_player($nick, $limit=10, $region='ru'){
+		$search_str = "http://api.worldoftanks.$region/community/accounts/api/1.1/?source_token=WG-WoT_Assistant-1.3.2&search=$nick&offset=0&limit=$limit";
+		$search_data = json_decode(file_get_contents ($search_str));
+		return $search_data->data;
+	}
+	
+	function player_info($id, $region='RU'){
+	
+	
+	}
+	
+}
+
+
+
+?>
