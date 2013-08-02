fec6e8573f308c7c032c996d595e579509d22afb
akeinhell <akeinhell@gmail.com>
2013-08-02 14:11:32 +0900

    -

--- wotapi.php
+++ wotapi.php
@@ -1,5 +1,4 @@
-<?
- 
+<? 
 class wotapi{
 	function search_player($nick, $limit=10, $region='ru'){
 		$search_str = "http://api.worldoftanks.$region/community/accounts/api/1.1/?source_token=WG-WoT_Assistant-1.3.2&search=$nick&offset=0&limit=$limit";
