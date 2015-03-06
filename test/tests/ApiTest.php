16053aa343c09aedd4e23526b5e6c0587c9538a0
Aleksey <akeinhell@gmail.com>
2015-03-06 13:38:05 +0700

    Добавлены тесты

---
+++ test/tests/ApiTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: akeinhell
+ * Date: 06.03.15
+ * Time: 13:35
+ */
+
+namespace tests;
+
+
+use WotApi\Api;
+
+class ApiTest extends \PHPUnit_Framework_TestCase {
+
+    function authTest()
+    {
+        Api::wot();
+    }
+}
+ 
